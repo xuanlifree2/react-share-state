244a2353ac30a217c05007fe0f7f73f2413bf121
xuanlifree2 <44470821+xuanlifree2@users.noreply.github.com>
2022-04-03 10:35:37 +0800

    update state

--- src/state.js
+++ src/state.js
@@ -1,17 +1,14 @@
-import { useCallback, useRef, useContext, createContext, useEffect } from 'react';
-import { Subject } from 'rxjs';
+import { createContext, useCallback, useContext, useEffect, useRef } from 'react';
+import { Subject, pipe, identity } from 'rxjs';
 import { filter, scan } from 'rxjs/operators';
 
-const buildState = (subject$, id, initValue) =>
-  subject$?.pipe(
-    filter(({ id: key }) => key === id),
-    // support function value as react setState: setSate(n=>n+1)
-    scan(([, prev], { value }) => [prev, typeof value === 'function' ? value(prev) : value], [undefined, initValue])
-    // startWith([undefined, initValue])
-  );
+const filterBy = (id) => pipe(filter(({ id: key }) => key === id));
 
-const subscribeState = (subject$, id, cb, initValue) =>
-  buildState(subject$, id, initValue).subscribe(([prev, current]) => cb?.(prev, current));
+// support function value as react setState: setSate(n=>n+1)
+const withPrev = () =>
+  pipe(scan(([, prev], { value }) => [prev, typeof value === 'function' ? value(prev) : value], []));
+
+const buildState = (id) => pipe(filterBy(id), withPrev());
 
 const Context = createContext();
 
@@ -22,33 +19,36 @@ export const StateProvider = ({ children }) => {
   return <Context.Provider value={subject$.current}>{children}</Context.Provider>;
 };
 
+export const useStream = (id, initValue, { prev = false } = {}) => {
+  const subject$ = useContext(Context);
+  return subject$.pipe(filterBy(id), prev ? withPrev() : identity());
+};
+
 export const useSet = (id) => {
   const subject$ = useContext(Context);
-  const setS = useCallback((value) => subject$?.next({ id, value }), [id, subject$]);
-  return setS;
+  return useCallback((value) => subject$?.next({ id, value }), [id, subject$]);
 };
 
 export const useGet = (id) => {
   const subject$ = useContext(Context);
   const value = useRef([]);
   useEffect(() => {
-    const sub = subscribeState(subject$, id, (prev, current) => {
-      value.current = [prev, current];
+    const sub = subject$.pipe(buildState(id)).subscribe((v) => {
+      value.current = v;
     });
     return () => sub.unsubscribe();
   }, [id, subject$]);
-
-  return useCallback(({ withPrev = false } = {}) => {
+  return useCallback(({ prev = false } = {}) => {
     const [, current] = value.current;
-    return withPrev ? value.current : current;
+    return prev ? value.current : current;
   }, []);
 };
 
 export const useSub = (id) => {
   const subject$ = useContext(Context);
   return useCallback(
-    (cb, { withPrev = false } = {}) => {
-      const s = subscribeState(subject$, id, (prev, current) => (withPrev ? cb?.(prev, current) : cb?.(current)));
+    (cb, { prev = false } = {}) => {
+      const s = subject$.pipe(buildState(id)).subscribe(([pre, cur]) => (prev ? cb?.([pre, cur]) : cb?.(cur)));
       return () => s.unsubscribe();
     },
     [id, subject$]
